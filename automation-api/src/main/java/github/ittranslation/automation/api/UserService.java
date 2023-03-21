ea9c21927615e4fba67980c956c7ffee77f16394
gaoxiang <630268696@qq.com>
2023-03-21 19:55:47 +0800

    Initial project - add UrlParse Module

---
+++ automation-api/src/main/java/github/ittranslation/automation/api/UserService.java
@@ -0,0 +1,14 @@
+package github.ittranslation.automation.api;
+
+
+import github.ittranslation.automation.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
---
+++ start/src/main/java/github/ittranslation/automation/AutomationApplication.java
@@ -0,0 +1,13 @@
+package github.ittranslation.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationApplication.class, args);
+    }
+
+}
