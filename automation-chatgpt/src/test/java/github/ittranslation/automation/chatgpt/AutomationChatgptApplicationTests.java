d759f9d4b07f892c07757be5d330999157488c21
zhangjiahao <1643938737@qq.com>
2023-03-23 16:27:05 +0800

    add：chatgpt翻译模块

---
+++ automation-chatgpt/src/test/java/github/ittranslation/automation/chatgpt/AutomationChatgptApplicationTests.java
@@ -0,0 +1,7 @@
+package github.ittranslation.automation.chatgpt;/*
+ * ClassName: AutomationChatgptApplicationTests
+ * Description: 
+ * @Author: zjh
+ * @Create: 2023/3/23
+ */public class AutomationChatgptApplicationTests {
+}
